b5acd91e4f9febd159896f5a76460e25ec4b2c1b
Андрей <admin@iMac-Andrej.local>
2021-03-24 11:53:47 +0300

    first commit

---
+++ app/src/main/java/com/example/prakt5/Main2Activity.kt
@@ -0,0 +1,34 @@
+package com.example.prakt5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+
+        val textView3 = findViewById<TextView>(R.id.textView3)
+
+
+        button.setOnClickListener {
+            val random = Random.nextInt(10, 99) + 1
+            val random2 = Random.nextInt(100, 999) + 1
+            textView.text = random.toString()
+            textView2.text = random2.toString()
+
+            val num1 = random
+            val num2 = random2
+            val sum1 = num1 + num2
+            textView3.text = "Сумма чисел = $sum1"
+        }
+    }
+}
